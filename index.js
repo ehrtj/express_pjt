691468ea89f175e4cb6cc2109f27af4371b5ad55
ehrtj <a01063006146@gmail.com>
2025-03-20 20:45:09 +0900

    db.post / db.get

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.listen(PORT, () => {
     console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
 });
 
-app.post("/articles", (req,res)=>{
+app.post('/articles', (req,res)=>{
+
     let {title , content} = req.body
 
     db.run(`INSERT INTO articles (title, content) VALUES (?, ?)`,
@@ -32,3 +33,40 @@ app.post("/articles", (req,res)=>{
     });
 })
 
+
+app.get('/articles', (req,res)=>{
+
+    //db.all은 반환되는것 + (여러가지 가져올 때 사용)
+    db.all('SELECT * FROM articles', [], (err, rows) => {
+        //err가 있다면 500err 매시지 주고 끝
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        //err가 없다면 이거 줌
+        res.json(rows);
+    });
+})
+
+app.get('/articles/:id', (req,res)=>{
+
+    //id 가져오기 (params 사용)
+    let id = req.params.id
+
+    //id에 해당하는 article만 리턴
+    //get은 한개 받아롤 때 사용
+    db.get('SELECT * FROM articles WHERE id = ?', [id], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        //!row = row가 없을 때
+        if (!row) {
+            return res.status(404).json({ error: 'Article not found' });
+        }
+        //1개이기 때문에 s빼기
+        res.json(row);
+    });
+
+})
+
+
+//https://kihyeonkwon.notion.site/1b9c3ce583dd808f9a18ec08c1437bcd
